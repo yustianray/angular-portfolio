6959e17d58a969560b2d714fa215701db4b76a40
Yustian Ray <ray.yustian@gmail.com>
2019-02-14 13:04:31 +0700

    Updates

---
+++ precache-manifest.e454d1ca2e0c173e6c1d3ec0ee565aff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "ec6240378491448dceed",
+    "url": "/portfolio/static/js/main.ec624037.chunk.js"
+  },
+  {
+    "revision": "6741ca9b79badb040ea5",
+    "url": "/portfolio/static/js/2.6741ca9b.chunk.js"
+  },
+  {
+    "revision": "ec6240378491448dceed",
+    "url": "/portfolio/static/css/main.579473f7.chunk.css"
+  },
+  {
+    "revision": "6741ca9b79badb040ea5",
+    "url": "/portfolio/static/css/2.dfe8e4f2.chunk.css"
+  },
+  {
+    "revision": "075b330f7a2a75f4d4c12ad7717c833b",
+    "url": "/portfolio/index.html"
+  }
+];
