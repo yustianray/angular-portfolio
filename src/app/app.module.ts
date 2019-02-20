6552867a598e0fa0f15af9e657e59311e1a1c2c1
18ray <ray.yustian@gmail.com>
2019-02-20 17:09:05 +0700

    new design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    ServicesComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
